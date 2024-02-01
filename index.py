cbde3fec99d7f2d4f28b7b51026f0c55f63457f9
zyhsck <158262486+zyhsck@users.noreply.github.com>
2024-02-01 17:31:06 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -10,8 +10,8 @@
 import tkinter as tk
 import os
 import tkinter.filedialog
-
-
+import wget
+import zipfile
 
 root = tk.Tk()
 root.title("EspTool")
@@ -24,9 +24,9 @@ except_text=""
 def get_id():
     global except_text
     exportation.delete(1.0,"end")
-    except_text=os.popen("python -m esptool -p COM"+com_entry.get()+" flash_id")
+    except_text=os.popen("python -m esptool -p COM"+com_entry.get()+" chip_id")
     
-    exportation.insert("end",str(except_text.read()))
+    exportation.insert("end",str(except_text.read())[0:1000])
 get_id_b =tk.Button(root,text="获取esp信息",command=get_id); get_id_b.place(x=90,y=0)
 #---------------------------------
 def get_file():
@@ -42,7 +42,7 @@ def flash_file():
         except_text=except_text.read()
         with open("console.txt","w") as f:
             f.write(except_text)
-        exportation.insert("end",except_text[-1:-500]+"\n省略\n结束")
+        exportation.insert("end",except_text[0:1000]+"\n省略\n结束")
 get_file_b =tk.Button(root,text="选择BIN文件",command=get_file); get_file_b.place(x=0,y=35)
 flash_file_b = tk.Button(root,text="烧录",command=flash_file); flash_file_b.place(x=85,y=35)
 #---------------------------------
@@ -52,15 +52,56 @@ def get_bin():
     except_text=except_text.read()
     with open("console.txt","w") as f:
             f.write(except_text)
-    exportation.insert("end",except_text[-1:-500]+"\n省略\n结束")
+    exportation.insert("end",except_text[0:1000]+"\n省略\n结束")
 
 get_bin_b = tk.Button(root,text="提取BIN文件",command=get_bin);get_bin_b.place(x=125,y=35)
 
-#----------------------------------
+#----------------------------------其它部分
+def other_com():
+    other_com_t=tk.Toplevel(root)
+
+    def readflash():
+        exportation.delete(1.0,"end")
+        print(com_entry.get())
+        except_text=os.popen("python -m esptool -p com"+com_entry.get()+" flash_id")
+        except_text=except_text.read()
+        exportation.insert("end",except_text[0:1000]+"\n省略\n结束")
+    def readstatus():
+        exportation.delete(1.0,"end")
+        print(com_entry.get())
+        except_text=os.popen("python -m esptool -p com"+com_entry.get()+" read_flash_status")
+        except_text=except_text.read()
+        exportation.insert("end",except_text[0:1000]+"\n省略\n结束")
+    t_readchip_b = tk.Button(other_com_t,text="读取flash",command=readflash);t_readchip_b.place(x=0,y=0)
+    t_readmem_b = tk.Button(other_com_t,text="读取status",command=readstatus);t_readmem_b.place(x=60,y=0)
+
+other_com_b = tk.Button(root,text="...",command=other_com);other_com_b.place(x=210,y=35)
+
+
+#---------------------------------------------------
+
+def get_new():
+    file_url = "https://codeload.github.com/zyhsck/esptool_tkinter/zip/refs/heads/main"
+    wget.download(file_url,"114514.zip")
+    file=zipfile.ZipFile("114514.zip")
+    file.extractall("")
+    file.close()
+    os.remove("114514.zip")
+    dir=os.listdir("esptool_tkinter-main")
+    for x  in dir:
+        
+        try:
+            try:
+                os.remove(x)
+            except:
+                pass
+            os.rename(r"esptool_tkinter-main/"+x,x)
+        except:
+            pass
+get_new_b =tk.Button(root,text="获取更新",command=get_new);get_new_b.place(x=200,y=450)
+
 
 tips_l = tk.Label(root,text="By 秃头程序园嘛~\nversion:1.1");tips_l.place(x=90,y=450)
-
-
 
 
 
